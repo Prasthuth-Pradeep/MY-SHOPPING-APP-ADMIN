0f8d752bf220f8b6290770555ca3a507bf308c28
prasthuth <prasthuth.pradeep@ostrumtech.com>
2022-08-30 22:47:11 +0530

    Added child routes inside the product
    Created the url
    'product/add' ,
    'product/list' ,
    'product/details'

--- src/app/sections/product/product-routing.module.ts
+++ src/app/sections/product/product-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [  {
-  path: '',
-  component: AddProductComponent
-}
+const routes: Routes = [
+  { path: 'add', component: AddProductComponent }, 
+  { path: 'list', component: ProductListComponent }, 
+  { path: 'details', component: ProductDetailsComponent },
+  { path: '**', redirectTo: 'add' },
 ];
 
 @NgModule({
